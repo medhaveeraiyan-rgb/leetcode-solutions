8c5398254424caa01804c95028446e4e0e716ff7
Medha V <medhaveeraiyan@gmail.com>
2026-08-23 12:11:56 +0530

    Implement groupAnagrams function in solution.py

---
+++ Arrays-and-Hashing/049-Group-Anagrams/solution.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for i in strs:
+            s = "".join(sorted(i))
+            groups[s].append(i)
+
+        return list(groups.values())
